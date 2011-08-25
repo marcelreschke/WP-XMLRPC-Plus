7d42bfd1edcb2d9c01c6843882064fa254e629b3
Marcel Reschke <marcel.reschke@gmail.com>
2011-08-25 14:19:47 +0200

    Adding wp-xmlrpc-plus.php

---
+++ wp-xmlrpc-plus.php
@@ -0,0 +1,12 @@
+<?php
+/*
+Plugin Name: WP XMLRPC Plus
+Plugin URI: http://github.com/reschkevito/WP-XMLRPC-Plus
+Description: A Wordpress Plugin which extends the XML RPC Server
+Version: 0.1
+Author: Marcel Reschke
+Author URI: http://github.com/reschkevito/
+*/
+
+
+?>
